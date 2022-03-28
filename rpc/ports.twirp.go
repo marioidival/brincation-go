e500870bd5834824a53eed7842d1fa0daf6e4d7e
Mario Idival <marioidival@gmail.com>
2022-03-28 14:57:58 -0300

    feat: add UpdatePort rpc method

--- rpc/ports.twirp.go
+++ rpc/ports.twirp.go
@@ -36,6 +36,8 @@ type PortService interface {
 	GetPort(context.Context, *PortId) (*Port, error)
 
 	CreatePort(context.Context, *Port) (*Port, error)
+
+	UpdatePort(context.Context, *UpdatePortRequest) (*Port, error)
 }
 
 // ===========================
@@ -44,7 +46,7 @@ type PortService interface {
 
 type portServiceProtobufClient struct {
 	client      HTTPClient
-	urls        [2]string
+	urls        [3]string
 	interceptor twirp.Interceptor
 	opts        twirp.ClientOptions
 }
@@ -72,9 +74,10 @@ func NewPortServiceProtobufClient(baseURL string, client HTTPClient, opts ...twi
 	// Build method URLs: <baseURL>[<prefix>]/<package>.<Service>/<Method>
 	serviceURL := sanitizeBaseURL(baseURL)
 	serviceURL += baseServicePath(pathPrefix, "brincation.ports", "PortService")
-	urls := [2]string{
+	urls := [3]string{
 		serviceURL + "GetPort",
 		serviceURL + "CreatePort",
+		serviceURL + "UpdatePort",
 	}
 
 	return &portServiceProtobufClient{
@@ -177,13 +180,59 @@ func (c *portServiceProtobufClient) callCreatePort(ctx context.Context, in *Port
 	return out, nil
 }
 
+func (c *portServiceProtobufClient) UpdatePort(ctx context.Context, in *UpdatePortRequest) (*Port, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "brincation.ports")
+	ctx = ctxsetters.WithServiceName(ctx, "PortService")
+	ctx = ctxsetters.WithMethodName(ctx, "UpdatePort")
+	caller := c.callUpdatePort
+	if c.interceptor != nil {
+		caller = func(ctx context.Context, req *UpdatePortRequest) (*Port, error) {
+			resp, err := c.interceptor(
+				func(ctx context.Context, req interface{}) (interface{}, error) {
+					typedReq, ok := req.(*UpdatePortRequest)
+					if !ok {
+						return nil, twirp.InternalError("failed type assertion req.(*UpdatePortRequest) when calling interceptor")
+					}
+					return c.callUpdatePort(ctx, typedReq)
+				},
+			)(ctx, req)
+			if resp != nil {
+				typedResp, ok := resp.(*Port)
+				if !ok {
+					return nil, twirp.InternalError("failed type assertion resp.(*Port) when calling interceptor")
+				}
+				return typedResp, err
+			}
+			return nil, err
+		}
+	}
+	return caller(ctx, in)
+}
+
+func (c *portServiceProtobufClient) callUpdatePort(ctx context.Context, in *UpdatePortRequest) (*Port, error) {
+	out := new(Port)
+	ctx, err := doProtobufRequest(ctx, c.client, c.opts.Hooks, c.urls[2], in, out)
+	if err != nil {
+		twerr, ok := err.(twirp.Error)
+		if !ok {
+			twerr = twirp.InternalErrorWith(err)
+		}
+		callClientError(ctx, c.opts.Hooks, twerr)
+		return nil, err
+	}
+
+	callClientResponseReceived(ctx, c.opts.Hooks)
+
+	return out, nil
+}
+
 // =======================
 // PortService JSON Client
 // =======================
 
 type portServiceJSONClient struct {
 	client      HTTPClient
-	urls        [2]string
+	urls        [3]string
 	interceptor twirp.Interceptor
 	opts        twirp.ClientOptions
 }
@@ -211,9 +260,10 @@ func NewPortServiceJSONClient(baseURL string, client HTTPClient, opts ...twirp.C
 	// Build method URLs: <baseURL>[<prefix>]/<package>.<Service>/<Method>
 	serviceURL := sanitizeBaseURL(baseURL)
 	serviceURL += baseServicePath(pathPrefix, "brincation.ports", "PortService")
-	urls := [2]string{
+	urls := [3]string{
 		serviceURL + "GetPort",
 		serviceURL + "CreatePort",
+		serviceURL + "UpdatePort",
 	}
 
 	return &portServiceJSONClient{
@@ -302,6 +352,52 @@ func (c *portServiceJSONClient) CreatePort(ctx context.Context, in *Port) (*Port
 func (c *portServiceJSONClient) callCreatePort(ctx context.Context, in *Port) (*Port, error) {
 	out := new(Port)
 	ctx, err := doJSONRequest(ctx, c.client, c.opts.Hooks, c.urls[1], in, out)
+	if err != nil {
+		twerr, ok := err.(twirp.Error)
+		if !ok {
+			twerr = twirp.InternalErrorWith(err)
+		}
+		callClientError(ctx, c.opts.Hooks, twerr)
+		return nil, err
+	}
+
+	callClientResponseReceived(ctx, c.opts.Hooks)
+
+	return out, nil
+}
+
+func (c *portServiceJSONClient) UpdatePort(ctx context.Context, in *UpdatePortRequest) (*Port, error) {
+	ctx = ctxsetters.WithPackageName(ctx, "brincation.ports")
+	ctx = ctxsetters.WithServiceName(ctx, "PortService")
+	ctx = ctxsetters.WithMethodName(ctx, "UpdatePort")
+	caller := c.callUpdatePort
+	if c.interceptor != nil {
+		caller = func(ctx context.Context, req *UpdatePortRequest) (*Port, error) {
+			resp, err := c.interceptor(
+				func(ctx context.Context, req interface{}) (interface{}, error) {
+					typedReq, ok := req.(*UpdatePortRequest)
+					if !ok {
+						return nil, twirp.InternalError("failed type assertion req.(*UpdatePortRequest) when calling interceptor")
+					}
+					return c.callUpdatePort(ctx, typedReq)
+				},
+			)(ctx, req)
+			if resp != nil {
+				typedResp, ok := resp.(*Port)
+				if !ok {
+					return nil, twirp.InternalError("failed type assertion resp.(*Port) when calling interceptor")
+				}
+				return typedResp, err
+			}
+			return nil, err
+		}
+	}
+	return caller(ctx, in)
+}
+
+func (c *portServiceJSONClient) callUpdatePort(ctx context.Context, in *UpdatePortRequest) (*Port, error) {
+	out := new(Port)
+	ctx, err := doJSONRequest(ctx, c.client, c.opts.Hooks, c.urls[2], in, out)
 	if err != nil {
 		twerr, ok := err.(twirp.Error)
 		if !ok {
@@ -418,6 +514,9 @@ func (s *portServiceServer) ServeHTTP(resp http.ResponseWriter, req *http.Reques
 		return
 	case "CreatePort":
 		s.serveCreatePort(ctx, resp, req)
+		return
+	case "UpdatePort":
+		s.serveUpdatePort(ctx, resp, req)
 		return
 	default:
 		msg := fmt.Sprintf("no handler for path %q", req.URL.Path)
@@ -763,6 +862,186 @@ func (s *portServiceServer) serveCreatePortProtobuf(ctx context.Context, resp ht
 	}
 	if respContent == nil {
 		s.writeError(ctx, resp, twirp.InternalError("received a nil *Port and nil error while calling CreatePort. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	respBytes, err := proto.Marshal(respContent)
+	if err != nil {
+		s.writeError(ctx, resp, wrapInternal(err, "failed to marshal proto response"))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/protobuf")
+	resp.Header().Set("Content-Length", strconv.Itoa(len(respBytes)))
+	resp.WriteHeader(http.StatusOK)
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		ctx = callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
+func (s *portServiceServer) serveUpdatePort(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	header := req.Header.Get("Content-Type")
+	i := strings.Index(header, ";")
+	if i == -1 {
+		i = len(header)
+	}
+	switch strings.TrimSpace(strings.ToLower(header[:i])) {
+	case "application/json":
+		s.serveUpdatePortJSON(ctx, resp, req)
+	case "application/protobuf":
+		s.serveUpdatePortProtobuf(ctx, resp, req)
+	default:
+		msg := fmt.Sprintf("unexpected Content-Type: %q", req.Header.Get("Content-Type"))
+		twerr := badRouteError(msg, req.Method, req.URL.Path)
+		s.writeError(ctx, resp, twerr)
+	}
+}
+
+func (s *portServiceServer) serveUpdatePortJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "UpdatePort")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	d := json.NewDecoder(req.Body)
+	rawReqBody := json.RawMessage{}
+	if err := d.Decode(&rawReqBody); err != nil {
+		s.handleRequestBodyError(ctx, resp, "the json request could not be decoded", err)
+		return
+	}
+	reqContent := new(UpdatePortRequest)
+	unmarshaler := protojson.UnmarshalOptions{DiscardUnknown: true}
+	if err = unmarshaler.Unmarshal(rawReqBody, reqContent); err != nil {
+		s.handleRequestBodyError(ctx, resp, "the json request could not be decoded", err)
+		return
+	}
+
+	handler := s.PortService.UpdatePort
+	if s.interceptor != nil {
+		handler = func(ctx context.Context, req *UpdatePortRequest) (*Port, error) {
+			resp, err := s.interceptor(
+				func(ctx context.Context, req interface{}) (interface{}, error) {
+					typedReq, ok := req.(*UpdatePortRequest)
+					if !ok {
+						return nil, twirp.InternalError("failed type assertion req.(*UpdatePortRequest) when calling interceptor")
+					}
+					return s.PortService.UpdatePort(ctx, typedReq)
+				},
+			)(ctx, req)
+			if resp != nil {
+				typedResp, ok := resp.(*Port)
+				if !ok {
+					return nil, twirp.InternalError("failed type assertion resp.(*Port) when calling interceptor")
+				}
+				return typedResp, err
+			}
+			return nil, err
+		}
+	}
+
+	// Call service method
+	var respContent *Port
+	func() {
+		defer ensurePanicResponses(ctx, resp, s.hooks)
+		respContent, err = handler(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *Port and nil error while calling UpdatePort. nil responses are not supported"))
+		return
+	}
+
+	ctx = callResponsePrepared(ctx, s.hooks)
+
+	marshaler := &protojson.MarshalOptions{UseProtoNames: !s.jsonCamelCase, EmitUnpopulated: !s.jsonSkipDefaults}
+	respBytes, err := marshaler.Marshal(respContent)
+	if err != nil {
+		s.writeError(ctx, resp, wrapInternal(err, "failed to marshal json response"))
+		return
+	}
+
+	ctx = ctxsetters.WithStatusCode(ctx, http.StatusOK)
+	resp.Header().Set("Content-Type", "application/json")
+	resp.Header().Set("Content-Length", strconv.Itoa(len(respBytes)))
+	resp.WriteHeader(http.StatusOK)
+
+	if n, err := resp.Write(respBytes); err != nil {
+		msg := fmt.Sprintf("failed to write response, %d of %d bytes written: %s", n, len(respBytes), err.Error())
+		twerr := twirp.NewError(twirp.Unknown, msg)
+		ctx = callError(ctx, s.hooks, twerr)
+	}
+	callResponseSent(ctx, s.hooks)
+}
+
+func (s *portServiceServer) serveUpdatePortProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	var err error
+	ctx = ctxsetters.WithMethodName(ctx, "UpdatePort")
+	ctx, err = callRequestRouted(ctx, s.hooks)
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+
+	buf, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		s.handleRequestBodyError(ctx, resp, "failed to read request body", err)
+		return
+	}
+	reqContent := new(UpdatePortRequest)
+	if err = proto.Unmarshal(buf, reqContent); err != nil {
+		s.writeError(ctx, resp, malformedRequestError("the protobuf request could not be decoded"))
+		return
+	}
+
+	handler := s.PortService.UpdatePort
+	if s.interceptor != nil {
+		handler = func(ctx context.Context, req *UpdatePortRequest) (*Port, error) {
+			resp, err := s.interceptor(
+				func(ctx context.Context, req interface{}) (interface{}, error) {
+					typedReq, ok := req.(*UpdatePortRequest)
+					if !ok {
+						return nil, twirp.InternalError("failed type assertion req.(*UpdatePortRequest) when calling interceptor")
+					}
+					return s.PortService.UpdatePort(ctx, typedReq)
+				},
+			)(ctx, req)
+			if resp != nil {
+				typedResp, ok := resp.(*Port)
+				if !ok {
+					return nil, twirp.InternalError("failed type assertion resp.(*Port) when calling interceptor")
+				}
+				return typedResp, err
+			}
+			return nil, err
+		}
+	}
+
+	// Call service method
+	var respContent *Port
+	func() {
+		defer ensurePanicResponses(ctx, resp, s.hooks)
+		respContent, err = handler(ctx, reqContent)
+	}()
+
+	if err != nil {
+		s.writeError(ctx, resp, err)
+		return
+	}
+	if respContent == nil {
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *Port and nil error while calling UpdatePort. nil responses are not supported"))
 		return
 	}
 
@@ -1364,25 +1643,28 @@ func callClientError(ctx context.Context, h *twirp.ClientHooks, err twirp.Error)
 }
 
 var twirpFileDescriptor0 = []byte{
-	// 316 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x92, 0x31, 0x6f, 0x22, 0x31,
-	0x10, 0x85, 0xb5, 0x0b, 0x2c, 0x30, 0x48, 0x77, 0x27, 0xeb, 0x84, 0x46, 0x54, 0x2b, 0x2a, 0x9a,
-	0xdb, 0x3d, 0x25, 0x65, 0x52, 0x25, 0x45, 0x94, 0x2e, 0x22, 0x5d, 0x3a, 0x63, 0x8f, 0xc8, 0x48,
-	0xac, 0x67, 0xe5, 0x35, 0x48, 0xe4, 0x17, 0xa4, 0xca, 0x6f, 0x8e, 0xd6, 0x0b, 0x24, 0x42, 0xa1,
-	0x9b, 0xf7, 0xbd, 0xb1, 0x47, 0xf3, 0x6c, 0xf8, 0xed, 0x6b, 0x53, 0xd6, 0xe2, 0x43, 0x53, 0xd4,
-	0x5e, 0x82, 0xa8, 0x3f, 0x2b, 0xcf, 0xce, 0xe8, 0xc0, 0xe2, 0x8a, 0xc8, 0xe7, 0x1f, 0x29, 0xf4,
-	0x9f, 0xc4, 0x07, 0xf5, 0x0b, 0x52, 0xb6, 0x98, 0xe4, 0xc9, 0x62, 0xbc, 0x4c, 0xd9, 0x2a, 0x05,
-	0x7d, 0xa7, 0x2b, 0xc2, 0x34, 0x92, 0x58, 0xb7, 0xcc, 0x70, 0xd8, 0x63, 0xaf, 0x63, 0x6d, 0xad,
-	0x10, 0x86, 0x46, 0xb6, 0x2e, 0xf8, 0x3d, 0xf6, 0x23, 0x3e, 0x4a, 0xf5, 0x17, 0x06, 0x7a, 0xc3,
-	0xba, 0xc1, 0x41, 0xde, 0x5b, 0x8c, 0x97, 0x9d, 0x68, 0xfb, 0x3d, 0xad, 0x59, 0x5c, 0x83, 0x59,
-	0xe4, 0x47, 0xa9, 0x72, 0x98, 0x18, 0x11, 0x6f, 0xd9, 0xe9, 0x40, 0x0d, 0x0e, 0xf3, 0xde, 0x22,
-	0x59, 0x7e, 0x47, 0x6a, 0x06, 0xa3, 0xda, 0xcb, 0x8e, 0x9d, 0x21, 0x1c, 0xc5, 0x61, 0x27, 0xdd,
-	0x7a, 0x81, 0x2b, 0x7a, 0x13, 0x47, 0x38, 0xee, 0xbc, 0xa3, 0x56, 0x53, 0xc8, 0xb6, 0x6e, 0x23,
-	0xa6, 0x41, 0x88, 0x23, 0x0f, 0x2a, 0xee, 0x23, 0x96, 0x70, 0x72, 0xd8, 0x47, 0x2c, 0xcd, 0x11,
-	0xb2, 0x36, 0x8f, 0x47, 0x7b, 0x9e, 0xc8, 0xd5, 0x7b, 0x02, 0x93, 0xd6, 0x7a, 0x26, 0xbf, 0x63,
-	0x43, 0xea, 0x06, 0x86, 0x0f, 0x14, 0x62, 0x78, 0x58, 0x9c, 0x07, 0x5b, 0x74, 0x97, 0xcc, 0xa6,
-	0x3f, 0x3b, 0xea, 0x16, 0xe0, 0xde, 0x93, 0x0e, 0x14, 0xd5, 0x85, 0xae, 0x4b, 0xa7, 0xef, 0xfe,
-	0xbf, 0x14, 0x6b, 0x0e, 0xaf, 0xdb, 0x55, 0x61, 0xa4, 0x2a, 0x2b, 0xed, 0x59, 0xd8, 0xf2, 0x4e,
-	0x6f, 0xca, 0xaf, 0xfe, 0x7f, 0x6b, 0x29, 0x4f, 0xef, 0xbf, 0xca, 0xe2, 0x07, 0xb8, 0xfe, 0x0c,
-	0x00, 0x00, 0xff, 0xff, 0xa7, 0x59, 0x43, 0xdb, 0x13, 0x02, 0x00, 0x00,
+	// 359 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x52, 0xcd, 0x4e, 0xf3, 0x30,
+	0x10, 0x54, 0xd2, 0x34, 0x6d, 0x37, 0xd2, 0xf7, 0x63, 0xa1, 0xca, 0xea, 0x29, 0x2a, 0x97, 0x0a,
+	0x89, 0x04, 0x95, 0x23, 0x9c, 0xe0, 0x80, 0x7a, 0x02, 0x05, 0x71, 0xe1, 0xe6, 0xda, 0x56, 0xb1,
+	0xd4, 0x78, 0x83, 0xe3, 0x54, 0x2a, 0x0f, 0xc1, 0xcb, 0xf1, 0x42, 0xc8, 0xee, 0x1f, 0x6a, 0xc9,
+	0xcd, 0x33, 0x3b, 0xbb, 0x9b, 0x99, 0x2c, 0xfc, 0x35, 0x15, 0xcf, 0x2b, 0x34, 0xb6, 0xce, 0x2a,
+	0x83, 0x16, 0xc9, 0xbf, 0xb9, 0x51, 0x9a, 0x33, 0xab, 0x50, 0x67, 0x9e, 0x1f, 0x7f, 0x86, 0x10,
+	0x3d, 0xa1, 0xb1, 0xe4, 0x0f, 0x84, 0x4a, 0xd0, 0x20, 0x0d, 0x26, 0x83, 0x22, 0x54, 0x82, 0x10,
+	0x88, 0x34, 0x2b, 0x25, 0x0d, 0x3d, 0xe3, 0xdf, 0x8e, 0xe3, 0xca, 0xae, 0x69, 0x67, 0xc3, 0xb9,
+	0x37, 0xa1, 0xd0, 0xe3, 0xd8, 0x68, 0x6b, 0xd6, 0x34, 0xf2, 0xf4, 0x0e, 0x92, 0x33, 0xe8, 0xb2,
+	0xa5, 0x62, 0x35, 0xed, 0xa6, 0x9d, 0xc9, 0xa0, 0xd8, 0x00, 0xa7, 0x37, 0x72, 0xa1, 0x50, 0xd7,
+	0x34, 0xf6, 0xfc, 0x0e, 0x92, 0x14, 0x12, 0x8e, 0x68, 0x84, 0xd2, 0xcc, 0xca, 0x9a, 0xf6, 0xd2,
+	0xce, 0x24, 0x28, 0x7e, 0x52, 0x64, 0x04, 0xfd, 0xca, 0xe0, 0x4a, 0x69, 0x2e, 0x69, 0xdf, 0x2f,
+	0xdb, 0x63, 0x57, 0xb3, 0xaa, 0x94, 0x1f, 0xa8, 0x25, 0x1d, 0x6c, 0x6a, 0x3b, 0x4c, 0x86, 0x10,
+	0x37, 0x7a, 0x89, 0xbc, 0xa6, 0xe0, 0x57, 0x6e, 0x91, 0xf7, 0x83, 0x42, 0xd2, 0x64, 0xeb, 0x07,
+	0x85, 0x1c, 0x53, 0x88, 0x5d, 0x1e, 0x33, 0x71, 0x9c, 0xc8, 0xf8, 0x11, 0xfe, 0xbf, 0x54, 0x82,
+	0x59, 0xe9, 0xea, 0x85, 0x7c, 0x6f, 0x64, 0x7d, 0x1a, 0xdb, 0x05, 0x44, 0x2e, 0x58, 0x1f, 0x5b,
+	0x32, 0x1d, 0x66, 0xc7, 0x81, 0x67, 0xbe, 0xd9, 0x6b, 0xa6, 0x5f, 0x01, 0x24, 0x0e, 0x3e, 0x4b,
+	0xb3, 0x52, 0x5c, 0x92, 0x1b, 0xe8, 0x3d, 0x48, 0xeb, 0xff, 0x06, 0xfd, 0xbd, 0x71, 0x26, 0x46,
+	0x2d, 0x23, 0xc9, 0x2d, 0xc0, 0xbd, 0x91, 0xdb, 0xaf, 0x23, 0x2d, 0xaa, 0xd6, 0xee, 0x19, 0xc0,
+	0xc1, 0x1b, 0x39, 0x3f, 0x55, 0x9d, 0x38, 0x6f, 0x1b, 0x75, 0x77, 0xf5, 0x9a, 0x2d, 0x94, 0x7d,
+	0x6b, 0xe6, 0x19, 0xc7, 0x32, 0x2f, 0x99, 0x51, 0xa8, 0x84, 0x5a, 0xb1, 0x65, 0x7e, 0xd0, 0x5f,
+	0x2e, 0x30, 0xdf, 0xdf, 0xe6, 0x3c, 0xf6, 0xc7, 0x79, 0xfd, 0x1d, 0x00, 0x00, 0xff, 0xff, 0x4b,
+	0xf3, 0xab, 0xd8, 0xaf, 0x02, 0x00, 0x00,
 }
